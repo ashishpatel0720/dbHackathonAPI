690a3c79a16ed599ba7e96a14bb28151be8579ea
ashish <ashishpatel0720@gmail.com>
2019-10-07 01:12:45 +0530

    [password]: lombok used

--- src/main/java/com/db/dbhackathonapi/Tables/Admin.java
+++ src/main/java/com/db/dbhackathonapi/Tables/Admin.java
@@ -1,10 +1,17 @@
 package com.db.dbhackathonapi.Tables;
 
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 
+@ToString
+@Setter
+@Getter
 @Entity // This tells Hibernate to make a table out of this class
 public class Admin {
 
@@ -13,36 +20,4 @@ public class Admin {
     private String name;
     private String password;
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	@Override
-	public String toString() {
-		return "Admin{" +
-				"id='" + id + '\'' +
-				", name='" + name + '\'' +
-				", password='" + password + '\'' +
-				'}';
-	}
 }
