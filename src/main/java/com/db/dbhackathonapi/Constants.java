7822cbf71a7fcca4690bde929969b22b79bf51a5
ashish <ashishpatel0720@gmail.com>
2019-10-09 14:28:16 +0530

    Flight added

--- src/main/java/com/db/dbhackathonapi/Constants.java
+++ src/main/java/com/db/dbhackathonapi/Constants.java
@@ -12,7 +12,7 @@ import static java.util.Comparator.comparing;
 
 public  final class Constants {
     public static final List<String> greenActivityList= Arrays.asList("Planted a Tree", "Used Public Transport","Used Paper Bags","Shared a Cab");
-    public static final List<String> travelActivityList= Arrays.asList("Bicycle", "Motor Bike","Car","Cab","Bus","Other");
+    public static final List<String> travelActivityList= Arrays.asList("Bicycle", "Motor Bike","Car","Flight","Cab","Bus","Other");
     public static final List<String> electricApplianceList= Arrays.asList("Fridge", "Ac","Washing Machine","Geyser","Other");
 
     //TODO: add comparator to all activity
